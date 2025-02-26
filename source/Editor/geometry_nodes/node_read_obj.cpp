e6ec5d2f572b68c6638c9c65e263b315346e9536
HuangWanchao <hwc20040629@mail.ustc.edu.cn>
2025-02-26 22:16:37 +0800

    Fix node_read_obj

--- source/Editor/geometry_nodes/node_read_obj.cpp
+++ source/Editor/geometry_nodes/node_read_obj.cpp
@@ -1,6 +1,9 @@
 
 #include <pxr/base/vt/array.h>
 
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+
 #include "GCore/Components/MeshOperand.h"
 #include "igl/readOBJ.h"
 #include "nodes/core/def/node_def.hpp"
@@ -86,6 +89,9 @@ NODE_EXECUTION_FUNCTION(read_obj_eigen)
 
 NODE_DECLARATION_UI(read_obj_eigen);
 
+// 定义一个简单的网格类型
+typedef OpenMesh::TriMesh_ArrayKernelT<> MyMesh;
+
 NODE_DECLARATION_FUNCTION(read_obj_pxr)
 {
     b.add_input<std::string>("Path").default_val("Default");
@@ -94,60 +100,101 @@ NODE_DECLARATION_FUNCTION(read_obj_pxr)
     b.add_output<pxr::VtArray<int>>("FaceVertexIndices");
     b.add_output<pxr::VtArray<pxr::GfVec3f>>("Normals");
     b.add_output<pxr::VtArray<pxr::GfVec2f>>("Texcoords");
-    // Function content omitted
 }
 
 NODE_EXECUTION_FUNCTION(read_obj_pxr)
 {
-    const auto path = params.get_input<std::string>("Path");
+    auto path = params.get_input<std::string>("Path");
     if (path.empty()) {
         return false;
     }
+    std::cout << path.length() << std::endl;
+    path = std::string(path.c_str());
+    std::cout << path.length() << std::endl;
 
-    Eigen::MatrixXf V, TC, N;
-    Eigen::MatrixXi F, FTC, FN;
+    MyMesh mesh;
+    OpenMesh::IO::Options opt;
+    opt += OpenMesh::IO::Options::VertexTexCoord;
+    opt += OpenMesh::IO::Options::VertexNormal;
 
-    if (!igl::readOBJ(path, V, TC, N, F, FTC, FN)) {
+    // 尝试读取OBJ文件
+    if (!OpenMesh::IO::read_mesh(mesh, path, opt)) {
         return false;
     }
 
-    pxr::VtVec3fArray vertices(V.rows());
-    Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, 3, Eigen::RowMajor>>
-        vertices_map(reinterpret_cast<float*>(vertices.data()), V.rows(), 3);
-    vertices_map = V;
+    // 请求法线和纹理坐标
+    if (opt.check(OpenMesh::IO::Options::VertexNormal)) {
+        mesh.request_vertex_normals();
+        if (!mesh.has_vertex_normals()) {
+            mesh.update_normals();
+        }
+    }
+    mesh.request_vertex_texcoords2D();
 
-    const int num_faces = F.rows();
-    const int vertices_per_face = F.cols();
+    // 转换顶点数据
+    pxr::VtVec3fArray vertices(mesh.n_vertices());
+    int vertex_idx = 0;
+    for (auto vh : mesh.vertices()) {
+        auto point = mesh.point(vh);
+        vertices[vertex_idx] = pxr::GfVec3f(point[0], point[1], point[2]);
+        vertex_idx++;
+    }
 
+    // 转换面数据
+    const int vertices_per_face = 3;  // 三角形网格
+    const int num_faces = mesh.n_faces();
     pxr::VtArray<int> faceVertexCounts(num_faces, vertices_per_face);
-
     pxr::VtArray<int> faceVertexIndices(num_faces * vertices_per_face);
-    Eigen::Map<Eigen::VectorXi>(faceVertexIndices.data(), F.size()) =
-        Eigen::Map<Eigen::VectorXi>(F.data(), F.size());
 
+    int face_idx = 0;
+    for (auto fh : mesh.faces()) {
+        int vertex_count = 0;
+        for (auto fv_it = mesh.fv_iter(fh); fv_it.is_valid(); ++fv_it) {
+            faceVertexIndices[face_idx * vertices_per_face + vertex_count] =
+                fv_it->idx();
+            vertex_count++;
+        }
+        face_idx++;
+    }
+
+    // 转换法线数据
     pxr::VtArray<pxr::GfVec3f> normals;
-    if (N.rows() > 0) {
-        normals.resize(N.rows());
-        Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, 3, Eigen::RowMajor>>
-            normals_map(reinterpret_cast<float*>(normals.data()), N.rows(), 3);
-        normals_map = N;
+    if (mesh.has_vertex_normals()) {
+        normals.resize(mesh.n_vertices());
+        int normal_idx = 0;
+        for (auto vh : mesh.vertices()) {
+            auto normal = mesh.normal(vh);
+            normals[normal_idx] = pxr::GfVec3f(normal[0], normal[1], normal[2]);
+            normal_idx++;
+        }
     }
 
+    // 转换纹理坐标数据
     pxr::VtArray<pxr::GfVec2f> texcoords;
-    if (TC.rows() > 0) {
-        texcoords.resize(TC.rows());
-        Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, 2, Eigen::RowMajor>>
-            texcoords_map(
-                reinterpret_cast<float*>(texcoords.data()), TC.rows(), 2);
-        texcoords_map = TC.leftCols(2);
+    if (mesh.has_vertex_texcoords2D()) {
+        texcoords.resize(mesh.n_vertices());
+        int texcoord_idx = 0;
+        for (auto vh : mesh.vertices()) {
+            auto texcoord = mesh.texcoord2D(vh);
+            texcoords[texcoord_idx] = pxr::GfVec2f(texcoord[0], texcoord[1]);
+            texcoord_idx++;
+        }
     }
 
+    // 设置输出
     params.set_output("Vertices", std::move(vertices));
     params.set_output("FaceVertexCounts", std::move(faceVertexCounts));
     params.set_output("FaceVertexIndices", std::move(faceVertexIndices));
     params.set_output("Normals", std::move(normals));
     params.set_output("Texcoords", std::move(texcoords));
 
+    // 清理请求的属性
+    mesh.release_vertex_normals();
+    mesh.release_vertex_texcoords2D();
+
     return true;
 }
+
+NODE_DECLARATION_UI(read_obj_pxr);
+
 NODE_DEF_CLOSE_SCOPE
